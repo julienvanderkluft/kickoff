b1cc9333e151c89a6ba8a33dfc7b4e88005f5239
Julien van der Kluft <julien@minuitsept.com>
2016-06-10 21:51:05 +0300

    Add sequence to build task, clean build target folder  before execute tasks

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,9 +2,16 @@ var gulp         = require('gulp')
     gutil        = require('gulp-util')
     plumber      = require('gulp-plumber')
     options      = require('minimist')(process.argv.slice(2))
+    sequence     = require('run-sequence');
 
 /* --
   gulp build task
 -- */
 
-gulp.task('build', ['copy', 'templates', 'styles', 'scripts', 'images', 'medias']);
+gulp.task('build', function(cb) {
+  sequence(
+    'clean:soft',
+    ['copy', 'templates', 'styles', 'scripts', 'images', 'medias'],
+    cb
+  );
+});
