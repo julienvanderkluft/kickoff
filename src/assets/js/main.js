e4be1aded56bdccfdf4812f7284a7b5121190b26
Julien van der Kluft <julien@minuitsept.com>
2016-06-12 20:24:11 +0300

    Init main js file

---
+++ src/assets/js/main.js
@@ -0,0 +1,21 @@
+window.$     = window.jQuery = require('jquery');
+var WebFont  = require('webfontloader');
+
+/*--
+  Exotic webfont loading
+--*/
+
+// WebFont.load({
+//   google: {
+//     families: [ '' ]
+//   }
+// });
+
+
+/* --
+  app
+--*/
+
+$(function() {
+
+});
