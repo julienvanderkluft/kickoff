27f31991ce3e261867243224670a8d56a689b58b
Julien van der Kluft <julien@dsply.fr>
2019-12-05 16:50:15 +0100

    👀

--- scripts/src/main.js
+++ scripts/src/main.js
@@ -1,5 +1,5 @@
 window.$     = window.jQuery = require('jquery');
-var WebFont  = require('webfontloader');
+// var WebFont  = require('webfontloader');
 
 /*--
   Exotic webfont loading
@@ -8,6 +8,54 @@ var WebFont  = require('webfontloader');
 // WebFont.load({
 //   google: {
 //     families: [ '' ]
+//   }
+// });
+
+
+/* --
+/* Global vars
+/* -- */
+
+if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
+  var local = true;
+}
+
+var isMobile = false;
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  isMobile = true;
+}
+
+//
+// Responsive breackpoints
+// var swapSize = 992;
+
+
+/* --
+/* Functions
+/* -- */
+
+// function function($el) {
+//   $el.each(function(index, iframe){
+//   });
+// }
+
+
+/* --
+/* Methods
+/* -- */
+
+//
+// Custom method
+// jQuery.fn.extend({
+//   method: function(options) {
+//     var settings  = $.extend({
+//           customOption : true,
+//         }, options),
+//         $items = $(this);
+
+//     $items.each(function() {
+//       var $el = $(this);
+//     });
 //   }
 // });
 
